bb3d5be31ea87a67662360ebe3e3b5e03d33a9b1
kevinlin <linjiang1205@gmail.com>
2022-10-05 16:40:23 +0800

    fix: dest路径错误

--- cmd/systrace-parser/main.go
+++ cmd/systrace-parser/main.go
@@ -49,6 +49,7 @@ func main() {
 }
 
 func OutputFilename(filename, inputDir, outputDir string) string {
+	log.Println("filename:", filename, ", inputDir:", inputDir, ", outputDir:", outputDir)
 	if inputDir == "." {
 		inputDir = "." + string(os.PathSeparator)
 	}
@@ -61,11 +62,13 @@ func OutputFilename(filename, inputDir, outputDir string) string {
 		outputDir += string(os.PathSeparator)
 	}
 
+	log.Println("after => filename:", filename, ", inputDir:", inputDir, ", outputDir:", outputDir)
 	f := strings.Replace(filename, inputDir, outputDir, -1)
 
 	filenameAll := path.Base(f)
 	fileExt := path.Ext(f)
-	return fmt.Sprintf("%s/%s%s", path.Dir(f), filenameAll[0:len(filenameAll)-len(fileExt)], ".txt")
+	log.Println("after => path:", path.Dir(f), filenameAll, fileExt)
+	return fmt.Sprintf("%s%s%s%s", path.Dir(f), string(os.PathSeparator), filenameAll[0:len(filenameAll)-len(fileExt)], ".txt")
 }
 
 // 解析一个systrace html文件并输出到指定位置
@@ -92,7 +95,7 @@ func ParseSysTrace(src, dest string) error {
 	}
 
 	dom.Find(".trace-data").Each(func(i int, s *goquery.Selection) {
-		fmt.Println("find trace-data")
+		// fmt.Println("find trace-data")
 		// res, _ := s.Html()
 		res := s.Text()
 		fmt.Printf("file: %s html size:%d\n", src, len(res))
